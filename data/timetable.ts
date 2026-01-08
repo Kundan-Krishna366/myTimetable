3589be4b3a44c88ff2d7cd86598874888c3f18bd
kundan-krishna366 <kundan.krishna366@gmail.com>
2026-01-08 13:44:28 +0530

    changes in schedule

--- data/timetable.ts
+++ data/timetable.ts
@@ -13,7 +13,7 @@ export const timetable: Timetable = {
     { time: "10:00 AM - 10:55 AM", entry: "P-25B17CI471 24A12 (HSL) CL9_1" },
     { time: "11:00 AM - 11:55 AM", entry: "P-25B17CI473 24A12 (RBT) CL6" },
     { time: "12:00 PM - 12:55 PM", entry: "P-25B17CI473 24A12 (RBT) CL6" },
-    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS434 (RTK) LT3" },
+    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
     { time: "04:00 PM - 04:55 PM", entry: "L-25B11CI413 24A11,24A12,24A13 (RBT) LT2" },
   ],
 
@@ -22,8 +22,8 @@ export const timetable: Timetable = {
     { time: "10:00 AM - 10:55 AM", entry: "L-25B1WCI431 24A11,24A12,24A13 (ATA) CR10" },
     { time: "11:00 AM - 11:55 AM", entry: "P-25B17CI476 24A12 (MGN) CL9_1" },
     { time: "12:00 PM - 12:55 PM", entry: "P-25B17CI476 24A12 (MGN) CL9_1" },
-    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS434 (RTK) LT2" },
-    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS434 (RTK) LT2" },
+    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
+    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
   ],
 
   WED: [
@@ -33,7 +33,7 @@ export const timetable: Timetable = {
     { time: "12:00 PM - 12:55 PM", entry: "L-25B11CI413 24A11,24A12,24A13 (RBT) LT2" },
     { time: "03:00 PM - 03:55 PM", entry: "P-25B1WCI471 B12[24A12] (ATA) CL5_2" },
     { time: "04:00 PM - 04:55 PM", entry: "P-25B1WCI471 B12[24A12] (ATA) CL5_2" },
-    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS434 (RTK) LT2" },
+    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
   ],
 
   THU: [
@@ -41,7 +41,7 @@ export const timetable: Timetable = {
     { time: "10:00 AM - 10:55 AM", entry: "L-25B11GE411 24A11,24A12,24A13 (TYN) CR8" },
     { time: "11:00 AM - 11:55 AM", entry: "L-25B11CI411 24A11,24A12,24A13 (ALK) CR2" },
     { time: "12:00 PM - 12:55 PM", entry: "L-25B11CI413 24A11,24A12,24A13 (RBT) CR7" },
-    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS434 (RTK) LT2" },
+    { time: "02:00 PM - 02:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
     { time: "03:00 PM - 03:55 PM", entry: "L-25B11CI412 24A11,24A12,24A13 (AKJ) CR7" },
   ],
 
@@ -51,7 +51,7 @@ export const timetable: Timetable = {
     { time: "11:00 AM - 11:55 AM", entry: "P-25B17CI472 24A12 (AKJ) CL4" },
     { time: "12:00 PM - 12:55 PM", entry: "P-25B17CI472 24A12 (AKJ) CL4" },
     { time: "04:00 PM - 04:55 PM", entry: "L-25B11GE411 24A11,24A12,24A13 (TYN) LT3" },
-    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS434 (RTK) LT2" },
+    { time: "05:00 PM - 05:55 PM", entry: "L-25B1WHS432 (DLR) CR9" },
   ],
 
   SAT: [
